b4f02a7f7178ec4999e5dbc274a0b033f5ddc2cd
Gabriel Fante <gabrielfante39@gmail.com>
2025-11-15 14:28:01 -0300

    first commit

---
+++ src/main/java/br/com/gabrielfante/API/modules/candidate/CandidateEntity.java
@@ -0,0 +1,26 @@
+package br.com.gabrielfante.API.modules.candidate;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.UUID;
+
+@Data
+public class CandidateEntity {
+
+    private UUID id;
+    private String name;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username]")
+    private String username;
+
+    @Email(message = "O campo [email] deve conter um e-mail válido")
+    private String email;
+
+    @Length(min = 10, max = 100)
+    private String password;
+    private String description;
+    private String curriculum;
+}
