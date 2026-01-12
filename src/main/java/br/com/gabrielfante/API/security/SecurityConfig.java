35edaeccdbc2b8f7e907263960c17936076ab554
GabrielFante <fantegabriel39@gmail.com>
2026-01-12 18:45:46 -0300

    Atualizando JobController

--- src/main/java/br/com/gabrielfante/API/security/SecurityConfig.java
+++ src/main/java/br/com/gabrielfante/API/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,11 +19,12 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
                         auth.requestMatchers("/candidate/").permitAll()
                                 .requestMatchers("/company/").permitAll()
-                                .requestMatchers("/auth/company").permitAll();
+                                .requestMatchers("/auth/company").permitAll()
+                                .requestMatchers("/job/").permitAll();
 
                         auth.anyRequest().authenticated();
                 })
