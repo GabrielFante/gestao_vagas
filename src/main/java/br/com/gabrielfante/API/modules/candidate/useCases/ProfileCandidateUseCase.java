607694f912e5786c8fcc442bfb9fc22ed17c8c70
GabrielFante <fantegabriel39@gmail.com>
2026-02-08 11:53:32 -0300

    Criando controller de Perfil do candidato

---
+++ src/main/java/br/com/gabrielfante/API/modules/candidate/useCases/ProfileCandidateUseCase.java
@@ -0,0 +1,35 @@
+package br.com.gabrielfante.API.modules.candidate.useCases;
+
+import br.com.gabrielfante.API.modules.candidate.CandidateRepository;
+import br.com.gabrielfante.API.modules.candidate.dto.ProfileCandidateResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.service.annotation.GetExchange;
+
+import java.util.UUID;
+
+@Service
+public class ProfileCandidateUseCase {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public ProfileCandidateResponseDTO execute(UUID idCandidate) {
+        var candidate = this.candidateRepository.findById(idCandidate)
+                .orElseThrow(() -> {
+                    throw new UsernameNotFoundException("User not found!");
+                });
+
+        var candidateDTO = ProfileCandidateResponseDTO.builder()
+                .description(candidate.getDescription())
+                .username(candidate.getUsername())
+                .email(candidate.getEmail())
+                .name(candidate.getName())
+                .id(candidate.getId())
+                .build();
+        return candidateDTO;
+    }
+}
